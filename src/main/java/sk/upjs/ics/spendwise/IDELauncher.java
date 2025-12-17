4a8c67d2267c4edff2f7897efb00d78c78e4a4d6
trm-oleksandr <kharchenkoalex94@gmail.com>
2025-12-17 18:38:26 +0100

    Add IDE launcher entry point

---
+++ src/main/java/sk/upjs/ics/spendwise/IDELauncher.java
@@ -0,0 +1,9 @@
+package sk.upjs.ics.spendwise;
+
+import sk.upjs.ics.financetracker.App;
+
+public class IDELauncher {
+    public static void main(String[] args) {
+        App.launch(App.class, args);
+    }
+}
