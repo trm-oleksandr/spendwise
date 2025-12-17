10a9f8b1c796431f3ef803f5e54d9a298b4e393a
trm-oleksandr <kharchenkoalex94@gmail.com>
2025-12-17 19:56:56 +0100

    Fix IDELauncher to reference SpendWise App

--- src/main/java/sk/upjs/ics/spendwise/IDELauncher.java
+++ src/main/java/sk/upjs/ics/spendwise/IDELauncher.java
@@ -1,6 +1,6 @@
 package sk.upjs.ics.spendwise;
 
-import sk.upjs.ics.financetracker.App;
+import sk.upjs.ics.spendwise.App;
 
 public class IDELauncher {
     public static void main(String[] args) {
